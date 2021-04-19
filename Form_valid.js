642cc9f7f5880a2cf509e17f9df5e8f09ba4635e
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-04-20 01:15:13 +0530

    Added form vaildation

---
+++ Form_valid.js
@@ -0,0 +1,19 @@
+function seterror(id, err){
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = err;
+
+}
+
+function validateForm(){
+    var returnval = true;
+    var Bname = document.forms['addBook']["BookName"].value;
+    if (Bname.length == 0){
+        seterror("bname", "*Book Name cannot be null!");
+        returnval = false;
+    }     
+    var Aname = document.forms['addBook']["BookAuthor"].value;
+    if (Aname.length == 0){
+        seterror("Aname", "*Book Author cannot be null!");
+        returnval = false;
+    }    return returnval;
+}
