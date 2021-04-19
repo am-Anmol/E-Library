7dbe5100e48826709384c9f1bf51b4bb9bf8b36e
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-04-19 11:35:08 +0530

    Minor changes in UI

--- Edit_Details.php
+++ Edit_Details.php
@@ -1,28 +1,3 @@
-<?php
-include 'config.php';
-
-if (isset($_GET['ISBN'])) {
-
-    $id = $_GET['ISBN'];
-	$sql = "SELECT * FROM books WHERE ISBN=$id";
-	$res = mysqli_query($conn, $sql);
-	$data = mysqli_fetch_assoc($res);
-    
-	if (isset($POST)) 
-    {
-		$name = $_POST['Name'];
-		$author_name = $_POST['Author'];
-		$description = $_POST['Description'];
-		$cover_image = $_POST['Cimage'];
-		if (isset($_POST["submit"])) 
-        {
-            $sql = "UPDATE  books SET Name='$name' , Author='$author_name' , Description='$description' , Cimage='$cover_image' WHERE ISBN='$id'";
-            $result = mysqli_query($conn, $sql);
-		}
-
-	}
-}
-?>
 
 <!doctype html>
 <html lang="en">
@@ -34,11 +9,12 @@ if (isset($_GET['ISBN'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Edit Book</title>
+    <link rel = "icon" href = "/elibrary/img/logo.svg" type = "image/x-icon">
 </head>
 
 <body style="background-color: #dee2e6;">
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="/elibrary/index.php">
                 <img src="img/logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"
@@ -54,8 +30,22 @@ if (isset($_GET['ISBN'])) {
         </div>
         </div>
     </nav>
-    <div class="container mt-4">
-        <form action="/elibrary/Edit_Details.php" method="POST">
+
+<?php
+ include('config.php');
+ $id = $_GET['ISBN'];
+ $sql = "SELECT * FROM books WHERE ISBN=$id";
+ $res=mysqli_query($conn,$sql);
+ 
+    
+  if(mysqli_num_rows($res)>0)
+  {
+    while($data = mysqli_fetch_assoc($res))
+    {
+?>
+
+<div class="container mt-4">
+        <form action="/elibrary/Update.php?ISBN=<?php echo $id ?>" method="POST">
             <div class="form-group mb-3 ">
                 <label for="BookName" class="form-label">Enter Book Name</label>
                 <input value="<?php echo $data['Name']; ?>" type="text" class="form-control" name="BookName" id="BookName" aria-describedby="emailHelp">
@@ -70,18 +60,24 @@ if (isset($_GET['ISBN'])) {
             </div>
             <div class="form-group mb-3">
                 <label for="BookDesc" class="form-label">Enter Book Description</label>
-                <textarea value="<?php echo $data['Description']; ?>" class="form-control"  name="BookDesc" id="BookDesc" cols="30" rows="6"></textarea>
+                <textarea  type="text" class="form-control"  name="BookDesc" id="BookDesc" cols="30" rows="6"><?php echo $data['Description']; ?></textarea>
             </div>
-            <button type="submit" name="edit" value="Edit" class="btn btn-primary ">Submit</button>
+            <button type="submit" name="Update" value="Update" class="btn btn-primary ">Update</button>
         </form>
       </div>
+    <?php
+      }
+    }
+    ?>
       
+      
+
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
         crossorigin="anonymous"></script>
 
-  
+
 
 </body>
 
