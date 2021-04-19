ab1ff5ab190d6c62d8ffc774933ccf3aacffa560
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-04-19 11:34:23 +0530

    Added pagination and serach bar

--- index.php
+++ index.php
@@ -1,27 +1,14 @@
-<?php 
-   include('config.php');
-   if ($_SERVER['REQUEST_METHOD'] == 'POST')
-   {
-   
-        $BookName=$_POST['BookName'];
-        $BookAuthor=$_POST['BookAuthor'];
-        $CoverImg=$_POST['CoverImg'];
-        $BookDesc=$_POST['BookDesc'];
-        $sql="INSERT INTO `books` (`Name`, `Author`, `Description`, `Cimage`) VALUES ('$BookName', '$BookAuthor','$BookDesc','$CoverImg')";
-        $res=mysqli_query($conn,$sql);
-        if($res){
-            echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>Book Inserted Successfully!</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>';
-        } 
-    }
-   
-    
+<?php
+include('config.php');
+$limit=3;
+if (isset($_GET['page']))
+{$page=$_GET['page'];}
+else{$page=1;}
+$offset=($page-1)*$limit;
 ?>
+
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -29,12 +16,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>E-Library</title>
+    <link rel = "icon" href = "/elibrary/img/logo.svg" type = "image/x-icon">
 </head>
-
-
 <body style="background-color: #dee2e6;">
-    <?php include 'config.php';?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="/elibrary/index.php">
                 <img src="img/logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"
@@ -47,30 +34,46 @@
             </button>
             <div class="collapse navbar-collapse " id="navbarNavDropdown">
                 <div class="navbar-nav me-auto mb-2 mb-lg-0"></div>
-                <form class="d-flex" >
-                    <input class="form-control  me-2" type="search" placeholder="Search" aria-label="Search">
+                <form class="d-flex" action="/elibrary/Search.php" name="addBook" method="GET">
+                    <input type='hidden'  name='page' value=<?php echo $page; ?>>
+                    <input class="form-control  me-2" type="search" name="Search" placeholder="Search" aria-label="Search">
+                    
                     <button class="btn btn-outline-success me-2" type="submit">Search</button>
-                    <button type="button" class="btn btn-outline-primary me-3 p-2"
-                    onclick="window.location.href='/elibrary/Add_Book.php';">AddBook</button>
                 </form>
-                
+                <div >
+                <button type="button" class="btn btn-outline-primary add-book"
+                    onclick="window.location.href='/elibrary/Add_Book.php';">Add Book</button>
+                </div>
             </div>
         </div>
     </nav>
+  <div class="container me-2 mt-2 d-flex justify-content-end "> 
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            Sort
+        </button>
+        <ul class="dropdown-menu " >
+            <li><a class="dropdown-item text-center" href="#">A-Z</a></li>
+            <li><a class="dropdown-item text-center" href="#">Z-A</a></li>
+        </ul>
+    </div>
+  </div>
+    
     <br>
      <div class="container ">
-        <div class='row row-cols-1 row-cols-md-3 g-3'>
+        <div class='row row-cols-1 row-cols-md-3 g-3 '>
      <?php 
-        $sq="SELECT * FROM `books`";
+        
+        $sq="SELECT * FROM `books` ORDER BY ISBN LIMIT $offset,$limit ";
         $r=mysqli_query($conn,$sq);
         if($r)
         {
             while($row = mysqli_fetch_assoc($r))
             { 
-                echo " <div class='col'>
+                echo " <div class='col '>
                 <div class='card text-center h-100 border-dark' >
                   <img src=".$row['Cimage']. " class='card-img-top' alt='Not avaible' style=' width='500' height='600';'>
-                      <div class='card-body '>
+                  <div class='card-body '>
                           <h5 class='card-title'>". $row['Name'] . "</h5>
                           <h5 class='card-author'>". $row['Author'] . "</h5>
                           <p class='card-text'>". $row['Description'] . "</p>
@@ -87,25 +90,45 @@
      ?>
         </div>
      </div>
-    
 
     <nav aria-label="Page navigation example">
-        <ul class="pagination justify-content-center p-2">
-            <li class="page-item disabled">
-                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#">Next</a>
-            </li>
-        </ul>
-    </nav>
+     <?php 
+         
+        $sql="SELECT * FROM `books`";
+        $result=mysqli_query($conn,$sql);
+        if(mysqli_num_rows($result)>0)
+        {
+            $total_record=mysqli_num_rows($result);
+            $total_page=ceil($total_record/$limit);
+            echo '<ul class="pagination justify-content-center p-2">';
+            if($page>1){
+                echo '<li class="page-item"><a class="page-link" href="/elibrary/index.php?page='.($page-1).'">&laquo;</a></li>';
+            }
+            
+            for($i=1;$i<=$total_page;$i++)
+            {  if($i==$page)
+                {$active="active";
+                }else{$active="";}
+               echo ' <li class="page-item '.$active.'"><a class="page-link" href="/elibrary/index.php?page='.$i.'">'.$i.'</a></li> ';
+            }
+            if($total_page>$page){
+            echo'<li class="page-item"><a class="page-link" href="/elibrary/index.php?page='.($page+1).'">&raquo;</a></li>';
+            }
+            echo '</ul>';
+        }
+    ?>
+  </nav>
+
+    
+
+  <footer class="footer mt-auto py-3 bg-light" style="text-align: center; " >
+    <div class="container" >
+        <span class="text-right">&#169;Copyright ColoredCow 2021</span>
+    </div>
+  </footer>
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
         crossorigin="anonymous"></script>
 </body>
-
 </html>
