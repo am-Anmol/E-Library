3d9ce85ce05ec70db4db9af8c1171072f540161f
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-04-19 11:36:37 +0530

    Changes in form

--- Add_Book.php
+++ Add_Book.php
@@ -1,4 +1,19 @@
-
+<?php 
+   include('config.php');
+   if ($_SERVER['REQUEST_METHOD'] == 'POST')
+   {
+   
+        $BookName=$_POST['BookName'];
+        $BookAuthor=$_POST['BookAuthor'];
+        $CoverImg=$_POST['CoverImg'];
+        $BookDesc=$_POST['BookDesc'];
+        $sql="INSERT INTO `books` (`Name`, `Author`, `Description`, `Cimage`) VALUES ('$BookName', '$BookAuthor','$BookDesc','$CoverImg')";
+        $res=mysqli_query($conn,$sql);
+        header("Location: /elibrary/index.php");        
+    }
+   
+    
+?>
 <!doctype html>
 <html lang="en">
 
@@ -9,11 +24,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Add Book</title>
+    <link rel = "icon" href = "/elibrary/img/logo.svg" type = "image/x-icon">
 </head>
 
 <body style="background-color: #dee2e6;">
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="/elibrary/index.php">
                 <img src="img/logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"
@@ -29,15 +45,18 @@
         </div>
         </div>
     </nav>
-    <div class="container mt-4">
-        <form action="/elibrary/index.php" method="POST">
-            <div class="form-group mb-3 ">
+    <div class="container mt-4 "  >
+        <form action="/elibrary/Add_Book.php" name="addBook" onsubmit="return validateForm()" method="POST">
+            <div class="form-group mb-3 " id="bname">
                 <label for="BookName" class="form-label">Enter Book Name</label>
-                <input type="text" class="form-control" name="BookName" id="BookName" aria-describedby="emailHelp">
+                <input type="text" class="form-control" name="BookName" id="BookName" >
+                <strong><span class="formerror alert-danger" ></span></strong>
+                
             </div>
-            <div class="form-group mb-3">
+            <div class="form-group mb-3" id="Aname">
                 <label for="BookAuthor" class="form-label">Enter Book Author</label>
-                <input type="text" class="form-control"  name="BookAuthor" id="BookAuthor">
+                <input type="text" class="form-control"  name="BookAuthor" id="BookAuthor" >
+                <strong><span class="formerror alert-danger" ></span></strong>
             </div>
             <div class="form-group mb-3">
                 <label for="CoverImg" class="form-label">Enter Book Cover Image</label>
@@ -47,15 +66,18 @@
                 <label for="BookDesc" class="form-label">Enter Book Description</label>
                 <textarea class="form-control"  name="BookDesc" id="BookDesc" cols="30" rows="6"></textarea>
             </div>
-            <button type="submit" class="btn btn-primary ">Submit</button>
+            <button type="submit" class="btn btn-primary " >Submit</button>
         </form>
       </div>
+
+   
       
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
-        crossorigin="anonymous"></script>
-
+        crossorigin="anonymous">
+    </script>
+    <script src="/elibrary/Form_Validate.js"></script>
   
 
 </body>
