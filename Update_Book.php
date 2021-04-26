56904108d1051ef552850c2bbf6c8d2269ace841
ANMOL GUPTA <guptaanmol248001@gmail.com>
2021-04-27 00:09:17 +0530

    Added file uploader functionality

--- Update_Book.php
+++ Update_Book.php
@@ -1,11 +1,13 @@
 <?php 
    include('config.php');
         $id=$_GET['ISBN'];
-        $BookName=$_POST['BookName'];
-        $BookAuthor=$_POST['BookAuthor'];
-        $CoverImg=$_POST['CoverImg'];
-        $BookDesc=$_POST['BookDesc'];
-        $sql="UPDATE books SET Name='$BookName',Author= '$BookAuthor' , Description='$BookDesc' ,Cimage='$CoverImg' WHERE ISBN='$id'";
+        $bookName=$_POST['bookName'];
+        $bookAuthor=$_POST['bookAuthor'];
+        $pname = "images/".rand(1000,10000)."-".$_FILES["image"]["name"];
+        $tname = $_FILES["image"]["tmp_name"];
+        move_uploaded_file($tname, $pname);
+        $bookDesc=$_POST['bookDesc'];
+        $sql="UPDATE books SET Name='$bookName',Author= '$bookAuthor' , Description='$bookDesc' ,Cimage='$pname' WHERE ISBN='$id'";
         $res=mysqli_query($conn,$sql);
         $msg="Book Edited Successfully";
         header("Location: /elibrary/View_Book_Details.php?ISBN=$id&msg=$msg");    
